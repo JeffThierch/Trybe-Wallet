35cbd220e1390c4c7af033a0a0627a0710f5978f
Jeff Thierch <jefferson08_alexandre@hotmail.com>
2022-01-10 19:56:26 -0300

    :sparkles: Feat: Implement Expenses Table Head

---
+++ src/components/ExpensesTable.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default function ExpensesTable() {
+  return (
+    <table>
+      <tr>
+        <td>Descrição</td>
+        <td>Tag</td>
+        <td>Método de pagamento</td>
+        <td>Valor</td>
+        <td>Moeda</td>
+        <td>Câmbio utilizado</td>
+        <td>Valor convertido</td>
+        <td>Moeda de conversão</td>
+      </tr>
+    </table>
+  );
+}
