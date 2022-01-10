91c4039f83a14ca5205a9e5d78967a70f2dc6768
Jeff Thierch <jefferson08_alexandre@hotmail.com>
2022-01-10 16:59:04 -0300

    :sparkles: Feat: Implement/Fix Validation in Login Page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,29 +1,36 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { setUserEmail } from '../actions';
 
 function Login() {
   const [isDisabled, toggleDisabled] = useState(true);
   const [userEmail, changeUserEmail] = useState('');
+  const [userPasswordLength, changeUserPassword] = useState(0);
+  const dispatch = useDispatch();
 
-  const checkIfEmailIsValid = () => {
+  const handleEmailChange = ({ target: { value } }) => {
+    changeUserEmail(value);
+  };
+
+  const handlePasswordChange = ({ target: { value } }) => {
+    changeUserPassword(value.length);
+  };
+
+  const handleSubmit = () => {
+    dispatch(setUserEmail(userEmail));
+  };
+
+  useEffect(() => {
+    const MIN_PASSWORD_LENGHT = 6;
     const emailRegex = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
-    const isValid = emailRegex.test(userEmail);
+    const isEmailValid = emailRegex.test(userEmail);
 
-    if (isValid) {
+    if (isEmailValid && userPasswordLength >= MIN_PASSWORD_LENGHT) {
       toggleDisabled(false);
     } else {
       toggleDisabled(true);
     }
-  };
-
-  const handleEmailChange = ({ target: { value } }) => {
-    console.log(value);
-    changeUserEmail(value);
-    checkIfEmailIsValid();
-  };
-
-  const handleSubmit = () => {
-
-  };
+  }, [userEmail, userPasswordLength]);
 
   return (
     <main>
@@ -41,6 +48,7 @@ function Login() {
             data-testid="password-input"
             placeholder="Insira sua senha"
             autoComplete="true"
+            onChange={ handlePasswordChange }
           />
         </div>
         <div>
