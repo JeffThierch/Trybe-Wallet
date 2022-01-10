b7fd3d7b938d1a992870f9369024b554c2f99c42
Jeff Thierch <jefferson08_alexandre@hotmail.com>
2022-01-10 16:12:01 -0300

    :sparkles: Feat: Login Page Structure and Validation

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Login() {
-  return <div>Login</div>;
+  const [isDisabled, toggleDisabled] = useState(true);
+  const [userEmail, changeUserEmail] = useState('');
+
+  const checkIfEmailIsValid = () => {
+    const emailRegex = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
+    const isValid = emailRegex.test(userEmail);
+
+    if (isValid) {
+      toggleDisabled(false);
+    } else {
+      toggleDisabled(true);
+    }
+  };
+
+  const handleEmailChange = ({ target: { value } }) => {
+    console.log(value);
+    changeUserEmail(value);
+    checkIfEmailIsValid();
+  };
+
+  const handleSubmit = () => {
+
+  };
+
+  return (
+    <main>
+      <form>
+        <div>
+          <input
+            type="email"
+            data-testid="email-input"
+            value={ userEmail }
+            placeholder="Insira seu email"
+            onChange={ handleEmailChange }
+          />
+          <input
+            type="password"
+            data-testid="password-input"
+            placeholder="Insira sua senha"
+            autoComplete="true"
+          />
+        </div>
+        <div>
+          <button type="button" disabled={ isDisabled } onClick={ handleSubmit }>Entrar</button>
+        </div>
+      </form>
+    </main>);
 }
 
 export default Login;
